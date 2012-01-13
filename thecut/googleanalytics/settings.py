8d93e003c0abe9f7dd68807b6a4590e14f70c2d9
Matt Austin <matt.austin@thecut.net.au>
2012-01-13 08:50:40 +0800

    Switched from pickling oauth token to gdata.gauth token_to_blob and token_from_blob.

--- thecut/googleanalytics/settings.py
+++ thecut/googleanalytics/settings.py
@@ -10,5 +10,5 @@ GDATA_CLIENT_SECRET = getattr(settings, 'GOOGLEANALYTICS_GDATA_CLIENT_SECRET',
 USE_GDATA = GDATA_CLIENT_ID is not None and GDATA_CLIENT_SECRET is not None
 
 USER_AGENT = getattr(settings, 'GOOGLEANALYTICS_USER_AGENT',
-    'thecut.googleanalytics/0.04 (The Cut; +http://www.thecut.net.au/')
+    'thecut.googleanalytics/0.041 (The Cut; +http://www.thecut.net.au/)')
 
