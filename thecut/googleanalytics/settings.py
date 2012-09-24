9d63c0ac3855efd1f56e9e1138aed098d3621e92
Matt Austin <matt.austin@thecut.net.au>
2012-09-24 14:15:28 +0800

    Removed ``GDATA`` settings.

--- thecut/googleanalytics/settings.py
+++ thecut/googleanalytics/settings.py
@@ -3,17 +3,10 @@ from __future__ import absolute_import, unicode_literals
 from django.conf import settings
 
 
-# GData settings are deprecated
-GDATA_CLIENT_ID = getattr(settings, 'GOOGLEANALYTICS_GDATA_CLIENT_ID', None)
-GDATA_CLIENT_SECRET = getattr(settings, 'GOOGLEANALYTICS_GDATA_CLIENT_SECRET',
-    None)
-USE_GDATA = None not in [GDATA_CLIENT_ID, GDATA_CLIENT_SECRET]
-
-
 GOOGLE_API_CLIENT_ID = getattr(settings,
-    'GOOGLEANALYTICS_GOOGLE_API_CLIENT_ID', GDATA_CLIENT_ID)
+    'GOOGLEANALYTICS_GOOGLE_API_CLIENT_ID', None)
 GOOGLE_API_CLIENT_SECRET = getattr(settings,
-    'GOOGLEANALYTICS_GOOGLE_API_CLIENT_SECRET', GDATA_CLIENT_SECRET)
+    'GOOGLEANALYTICS_GOOGLE_API_CLIENT_SECRET', None)
 USE_GOOGLE_API = None not in [GOOGLE_API_CLIENT_ID, GOOGLE_API_CLIENT_SECRET]
 
 
